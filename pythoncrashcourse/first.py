bac4e5b18c0193c2ffff371395dec603079ba9a9
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-03-30 02:16:52 +0600

    type

--- pythoncrashcourse/first.py
+++ pythoncrashcourse/first.py
@@ -1,5 +1,8 @@
 message = ' trying single and "double" commas '
 print(message)
+print(type(message))
+a = input("your name?")
+print(a)
 message2 = " hi"
 print (f"{message} concateneted \n {message2}")
-2 3 4
+# 2 3 4
